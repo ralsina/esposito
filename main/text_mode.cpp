86beb71785c0b0249e03c5c9d231e555d35acf2b
Roberto Alsina <roberto.alsina@gmail.com>
2026-05-15 17:22:06 -0300

    text_mode: render cells with null-terminated single-char buffer

--- main/text_mode.cpp
+++ main/text_mode.cpp
@@ -65,9 +65,10 @@ static void update_cell(int x, int y) {
     display_fill_rect(px, py, font_width, font_height, bg);
 
     if (cell->character != ' ') {
-        display_draw_text_bg(px, py, &cell->character, fg, bg);
+        char glyph[2] = { cell->character, '\0' };
+        display_draw_text_bg(px, py, glyph, fg, bg);
         if (cell->attributes & TEXT_ATTR_BOLD) {
-            display_draw_text_transparent(px + 1, py, &cell->character, fg);
+            display_draw_text_transparent(px + 1, py, glyph, fg);
         }
     }
 
