dfc47eeac208f6e2018455d022aefe8db4e4ea2a
Roberto Alsina <roberto.alsina@gmail.com>
2026-06-10 00:15:03 -0300

    Add file manager icons and regenerate fonts
    - Add file-plus, folder-plus, edit-2, copy, trash-2 to Lucide icon set
    - Update file_manager app with correct icon UTF-8 escape sequences
    - Regenerate all fonts with 51 Lucide icons
    - Fix first three file manager buttons: file-plus, folder-plus, edit-2

--- apps/file_manager/app.c
+++ apps/file_manager/app.c
@@ -868,13 +868,13 @@ void app_init(app_context_t *ctx) {
 
     int btn_w = cols / 7;
     struct { const char *label; void (*cb)(ui2_button_t *, void *); } btn_defs[] = {
-        {"\xee\x83\x86", on_new_file_click},
-        {"\xee\x83\x97", on_mkdir_click},
-        {"\xee\x83\x86", on_rename_click},
-        {"\xee\x81\xaf", on_copy_click},
-        {"\xee\x83\x89", on_delete_click},
-        {"\xee\x81\xbc", on_open_click},
-        {"\xee\x82\x84", on_exit_click},
+        {"\xe0\xc9", on_new_file_click},
+        {"\xe0\xd9", on_mkdir_click},
+        {"\xe1\x2f", on_rename_click},
+        {"\xe1\x2a", on_copy_click},
+        {"\xe1\x2d", on_delete_click},
+        {"\xe0\xce", on_open_click},
+        {"\xe0\x84", on_exit_click},
     };
     for (int i = 0; i < 7; i++) {
         ui2_button_t *btn = ui2_button_create(0, 0, btn_w, 3, btn_defs[i].label);
