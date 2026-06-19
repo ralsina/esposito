da5276fb375c172e4ec061c9036746e39bf58381
Roberto Alsina <roberto.alsina@gmail.com>
2026-06-18 21:21:58 -0300

    Bump version to v0.5.3

--- main/main.c
+++ main/main.c
@@ -9,7 +9,7 @@ static const char *TAG = "esposito";
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "Esposito OS v0.5.2");
+    ESP_LOGI(TAG, "Esposito OS v0.5.3");
     ESP_LOGI(TAG, "Starting boot sequence...");
 
     // Force floating-point library functions to be linked for C++ apps
